c9d2e6499d676f018242a035d29f1d55a4c302ab
azzed <azzed@DESKTOP-3JA9ICU>
2016-09-01 09:18:41 +0200

    Redaction des tests vides du menu

--- test/Draggenda/MenuTest.java
+++ test/Draggenda/MenuTest.java
@@ -6,8 +6,30 @@ import org.junit.Test;
 
 public class MenuTest {
 
+
 	@Test
-	public void test() {
+	public void Showagenda() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void CreateEvent() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void SearchUsers() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void afficherMenu() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void SaisieNombre() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void LancerAction() {
 		fail("Not yet implemented");
 	}
 
