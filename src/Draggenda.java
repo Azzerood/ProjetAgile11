ee1f8930281d0d550b2eb686379da4945a053738
PC Dylan <PC Dylan@Dylan>
2016-09-01 09:02:25 +0200

    Classe de notre menu d'actions pour l'instant vide avec méthodes

--- src/Draggenda.java
+++ src/Draggenda.java
@@ -4,5 +4,4 @@ public class Draggenda {
 	public static void main(String[] args) {
 		System.out.println("Hello World !!!");
 	}
-
 }
---
+++ src/Menu.java
@@ -0,0 +1,19 @@
+
+public class Menu {
+	
+	public Menu(){
+			
+	}
+	public void Showagenda(){
+		
+	}
+	public void CreateEvent(){
+		
+	}
+	public void SearchUsers(){
+		
+	}
+	public void AfficherMenu(){
+		
+	}
+}
