7769b7a431208a353617f8bb72f5789236273f8e
azzed <azzed@DESKTOP-3JA9ICU>
2016-09-01 11:07:54 +0200

    Correction test Menu

--- test/Draggenda/MenuTest.java
+++ test/Draggenda/MenuTest.java
@@ -10,14 +10,14 @@ public class MenuTest {
 	
 	
 	@Test
-	public void toString() {
+	public void afficherMenu() {
 		assertEquals(new Menu().toString(), "Votre Menu d'actions\n1- Consulter votre agenda\n2-Créer un évènement\n3-Recherche d'utilisateurs");
 	}
 	
 	@Test
 	public void verifNombre() {
 		assertEquals( new Menu().VerifNombre("toto"),false );
-		asserEquals( new Menu().VerifNombre("1039"), 1039);
+		assertEquals( new Menu().VerifNombre("1039"), 1039);
 		
 	}
 	
