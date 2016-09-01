e56c8b1b070fed62d717b35a331a07d5885ca95c
Enileme <dieu.emeline@gmail.com>
2016-09-01 10:02:18 +0200

    Modif Evenement

--- src/Draggenda/Draggenda.java
+++ src/Draggenda/Draggenda.java
@@ -1,8 +1,14 @@
 package Draggenda;
 
+import java.util.ArrayList;
+
 public class Draggenda {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !!!");
+		ArrayList<String> participantsTest = new ArrayList<String>();
+		participantsTest.add("nom1");
+		participantsTest.add("nom2");
+		Evenement testEven = new Evenement("nomTest","desriptionTest","2016/09/01","heureDepTest","heureFinTest", participantsTest);
+		System.out.println(testEven.toString());
 	}
 }
--- src/Draggenda/Evenement.java
+++ src/Draggenda/Evenement.java
@@ -1,9 +1,30 @@
 package Draggenda;
 
+import java.util.ArrayList;
+
 public class Evenement {
 
-	public static void main(String[] args) {
-		
+
+	private String nom;
+	private String description;
+	private String date;
+	private String heureDepart, heureFin;
+	private ArrayList<String> participants;
+	
+	public Evenement(String nom, String description, String date, String heureDepart, String heureFin, ArrayList<String> participants){
+		this.nom=nom;
+		this.description=description;
+		this.date=date;
+		this.heureDepart=heureDepart;
+		this.heureFin=heureFin;
+		this.participants=participants;
 	}
+
+	public String toString() {
+		return "Nom : " + nom + "\nDescription : " + description + "\nDate : " + date + "\nHeureDepart : "
+				+ heureDepart + "\nHeureFin : " + heureFin + "\nParticipants : " + participants + ".";
+	}
+
+	
 
 }
