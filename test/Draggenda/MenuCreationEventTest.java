26286e1cf8dcb92a3ff0170bd47e0d1fdcaadebf
azzed <azzed@DESKTOP-3JA9ICU>
2016-09-01 15:17:04 +0200

    Correction Menucreation et test associés

--- test/Draggenda/MenuCreationEventTest.java
+++ test/Draggenda/MenuCreationEventTest.java
@@ -8,24 +8,29 @@ public class MenuCreationEventTest {
 
 	@Test
 	public void VerifJour() {
-		fail("Not yet implemented");
+		assertEquals(new MenuCreationEvent().VerifJour("12"), 12);
+		assertEquals(new MenuCreationEvent().VerifJour("1"), 1);
+		assertEquals(new MenuCreationEvent().VerifJour("01"), 1);
+		assertEquals(new MenuCreationEvent().VerifJour("-5"), -1);
+		assertEquals(new MenuCreationEvent().VerifJour("32"), 32);
 	}
 	@Test
 	public void VerifMois() {
-		fail("Not yet implemented");
+		assertEquals(new MenuCreationEvent().VerifMois("12"), 12);
+		assertEquals(new MenuCreationEvent().VerifMois("1"), 1);
+		assertEquals(new MenuCreationEvent().VerifMois("01"), 1);
+		assertEquals(new MenuCreationEvent().VerifMois("-5"), -1);
+		assertEquals(new MenuCreationEvent().VerifMois("15"), 15);
 	}
 	@Test
 	public void  VerifAnnee() {
-		fail("Not yet implemented");
+		assertEquals(new MenuCreationEvent().VerifAnnee("123"), -1);
+		assertEquals(new MenuCreationEvent().VerifAnnee("2016"), 2016);
+		assertEquals(new MenuCreationEvent().VerifAnnee("2015"), 2015);
+		assertEquals(new MenuCreationEvent().VerifAnnee("2017"), 2017);
+		assertEquals(new MenuCreationEvent().VerifAnnee("20000"), -1);
 	}
-	@Test
-	public void  VerifDateFin() {
-		fail("Not yet implemented");
-	}
-	@Test
-	public void  VerifHeure() {
-		fail("Not yet implemented");
-	}
+	
 	
 	
 }
