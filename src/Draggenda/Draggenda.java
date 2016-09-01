baae8c4bd09868262ce5c05c78283004ff53bfd2
azzed <azzed@DESKTOP-3JA9ICU>
2016-09-01 16:07:04 +0200

    menu principal

--- src/Draggenda/Draggenda.java
+++ src/Draggenda/Draggenda.java
@@ -8,7 +8,7 @@ public class Draggenda {
 		
 		
 		ArrayList<String> participantsTest = new ArrayList<String>();
-		participantsTest.add("nom1");
+		participantsTest.add("nom1"); 
 		participantsTest.add("nom2");
 		Evenement testEvent = new Evenement("nomTest","desriptionTest1",new Date(01,9,2016),new Date(01,9,2016),new Heure(10,00),new Heure(11,00), participantsTest,true);
 		System.out.println(testEvent.toString());
