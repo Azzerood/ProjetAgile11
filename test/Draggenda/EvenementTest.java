91014ec9d2bb177ef11c8c4646577540576bebb3
Gaby <Gaby@DESKTOP-NQ8TNGG>
2016-09-01 10:36:22 +0200

    main+test event

--- test/Draggenda/EvenementTest.java
+++ test/Draggenda/EvenementTest.java
@@ -1,12 +1,18 @@
 package Draggenda;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class EvenementTest {
 
 	@Test
 	public void toStringTest() { 
-		Evenement event=new Evenement(); 
+		ArrayList <String> particip=new ArrayList<>();
+		particip.add("participant test");
+		Evenement event=new Evenement("Test","Description du Test","01/09/2016",new Heure(9,00),new Heure(10,00),particip);
+		assertEquals("Nom : Test\nDescription : Description du Test\nDate : 01/09/2016\nHeureDepart : 9:00\nHeureFin : 10:00\nParticipants : [participant test].",event.toString());
 	}
 
 }
