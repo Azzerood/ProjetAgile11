d01f4f7a07e55da8909b681d7eddd3cc910a9e32
raababouh <raababouh@mbp-de-rabah>
2016-09-01 15:39:21 +0200

    Menu events

--- src/Draggenda/Menu.java
+++ src/Draggenda/Menu.java
@@ -20,42 +20,6 @@ public class Menu {
 	public void SearchUsers() {
 	}
 
-	public String SearchByEvent() {
-		System.out.println("Saisir l'évenement à rechercher");
-		String rep = sc.nextLine();
-		return rep;
-	}
-
-	public Date SearchByDateDepart() {
-		System.out.println("Saisir la date voulu sour le format JJ/MM/AAAA");
-		System.out.println("Ne pas écrire les zéros inutils");
-		System.out.println("Saisir le jour");
-		int jour = sc.nextInt();
-		System.out.println("Saisir le mois");
-		int mois = sc.nextInt();
-		System.out.println("Saisir le annee");
-		int annee = sc.nextInt();
-		return new Date(jour, mois, annee);
-	}
-
-	public void ModifEvent(Evenement e) {
-		System.out.println("Voulez-vous modifier un Evenement ? (1=Oui)");
-		int rep = sc.nextInt();
-		if (rep == 1) {
-			e.modifier();
-		}
-	}
-
-	public boolean DeleteEvent() {
-		System.out.println("Voulez-vous supprimer un evenement ? (1=Oui)");
-		int rep = sc.nextInt();
-		if (rep == 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 	public String SaisieNombre() {
 		String nombre;
 
--- src/Draggenda/MenuEvent.java
+++ src/Draggenda/MenuEvent.java
@@ -9,22 +9,40 @@ public class MenuEvent {
 	public MenuEvent() {
 		sc = new Scanner(System.in);
 	}
-
-	public void searchEvent(Evenement e) {
-		// COMPLETER
+	
+	public String SearchByEvent() {
+		System.out.println("Saisir l'évenement à rechercher");
+		String rep = sc.nextLine();
+		return rep;
 	}
 
-	public Evenement afficherEvent(Evenement e) {
-		// COMPLETER
-		return null;
+	public Date SearchByDateDepart() {
+		System.out.println("Saisir la date voulu sour le format JJ/MM/AAAA");
+		System.out.println("Ne pas écrire les zéros inutils");
+		System.out.println("Saisir le jour");
+		int jour = sc.nextInt();
+		System.out.println("Saisir le mois");
+		int mois = sc.nextInt();
+		System.out.println("Saisir le annee");
+		int annee = sc.nextInt();
+		return new Date(jour, mois, annee);
 	}
 
-	public void modifierEvent() {
-		// COMPLETER
+	public void ModifEvent(Evenement e) {
+		System.out.println("Voulez-vous modifier un Evenement ? (1=Oui)");
+		int rep = sc.nextInt();
+		if (rep == 1) {
+			e.modifier();
+		}
 	}
 
-	public void modifierEvent(Evenement e) {
-		// COMPLETER
+	public boolean DeleteEvent() {
+		System.out.println("Voulez-vous supprimer un evenement ? (1=Oui)");
+		int rep = sc.nextInt();
+		if (rep == 1) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-
 }
