158f10827ee187eabf75812f426107691a91f927
Gaut-vador <Gaut-vador@GLaDOS-PE>
2016-09-01 09:12:49 +0200

    Agenda

---
+++ src/Agenda.java
@@ -0,0 +1,15 @@
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+
+
+public class Agenda {
+
+	//liste d'evenements
+	private Evenement[] e = new Evenement[24];
+	private HashMap<GregorianCalendar, Evenement> calendrier = new HashMap<>();
+	private GregorianCalendar dateActu = new GregorianCalendar();
+	
+	
+	
+	
+}
