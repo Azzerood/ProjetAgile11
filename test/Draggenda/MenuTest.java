a61673f48c3a54a9868870f886092c7e22843310
azzed <azzed@DESKTOP-3JA9ICU>
2016-09-01 09:21:30 +0200

    Modifs test unitaire menu

--- test/Draggenda/MenuTest.java
+++ test/Draggenda/MenuTest.java
@@ -11,14 +11,7 @@ public class MenuTest {
 	public void Showagenda() {
 		fail("Not yet implemented");
 	}
-	@Test
-	public void CreateEvent() {
-		fail("Not yet implemented");
-	}
-	@Test
-	public void SearchUsers() {
-		fail("Not yet implemented");
-	}
+	
 	@Test
 	public void afficherMenu() {
 		fail("Not yet implemented");
@@ -28,9 +21,5 @@ public class MenuTest {
 	public void SaisieNombre() {
 		fail("Not yet implemented");
 	}
-	@Test
-	public void LancerAction() {
-		fail("Not yet implemented");
-	}
-
+	
 }
