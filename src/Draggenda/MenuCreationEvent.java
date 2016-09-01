eb736b1a515f67bb90ec735a88b84bba1680cad0
PC Dylan <PC Dylan@Dylan>
2016-09-01 14:14:34 +0200

    menucrea

--- src/Draggenda/MenuCreationEvent.java
+++ src/Draggenda/MenuCreationEvent.java
@@ -24,7 +24,7 @@ public class MenuCreationEvent {
 	}
 	public int VerifJour(String day){
 		int rep=-1;
-		if(day.matches("") && rep <= 31 ){
+		if(day.matches("[0-31]")){
 			rep=Integer.parseInt(day);
 		}
 		return rep;
@@ -34,22 +34,33 @@ public class MenuCreationEvent {
 		String reponse=sc.nextLine();
 		return reponse;
 	}
-	public void VerifMois(String month){
-		
+	public int VerifMois(String month){
+		int rep=-1;
+		if(month.matches("[0-12]")){
+			rep=Integer.parseInt(month);
+		}
+		return rep;
 	}
 	public String SaisirAnnee(){
 		System.out.println("Veuillez saisir l'année");
 		String reponse=sc.nextLine();
 		return reponse;
 	}
-	public void VerifAnnee(){
-		
+	public int VerifAnnee(String year){
+		int rep=-1;
+		if(year.matches("[0-9999]")){
+			rep=Integer.parseInt(year);
+		}
+		return rep;
 	}
 	public Evenement CreeEvent(){
 		String nom=SaisirNom();
 		String descriptino=SaisirDescription();
-		int jour=SaisirJour();
-		
+		String jour=SaisirJour();
+		String mois=SaisirMois();
+		String annee=SaisirAnnee();
+		String heure=SaisirHeure();
+		String minute=SaisirMinute();
 		
 	}
 	public String SaisirHeure(){
@@ -58,23 +69,31 @@ public class MenuCreationEvent {
 		return reponse;
 		
 	}
-	
-
-	public void VerifHeure(String hour){
-		int rep=-1;
-		if(rep >= 0 && rep <= 12){
-			rep=Integer.parseInt(hour);
-			
-		}
-		String reponse1;
-		reponse1=SaisirHeure()+":"+SaisirMinute();
+	public String SaisirMinute(){
+		System.out.println("Veuillez choisir les minutes");
+		String reponse=sc.nextLine();
+		return reponse;
 	}
-	public void VerifHeure(){
-
-		
+	
+	public int VerifHeure(String hour){
+		int rep=-1;
+		if(hour.matches("[0-24]")){
+			rep=Integer.parseInt(hour);
+		}
+		return rep;
+		//String reponse1;
+		//reponse1=SaisirHeure()+":"+SaisirMinute();
+	}
+	public int VerifMinute(String min){
+		int rep=-1;
+		if(min.matches("[0-60]")){
+			rep=Integer.parseInt(min);
+		}
+		return rep;
 	}
 
 	public void VerifHeureFin(Heure debut, Heure fin){
+		
 	}
 	public void VerifHeureFin(){
 
