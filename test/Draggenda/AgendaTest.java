812959f17993bce7afc6adf9df39fe11e1bdaaa6
Gaut-vador <Gaut-vador@GLaDOS-PE>
2016-09-01 09:16:54 +0200

    AgendaTest

---
+++ test/Draggenda/AgendaTest.java
@@ -0,0 +1,14 @@
+package Draggenda;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AgendaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
