757bdfc1ec028b11afc3e267b8a89bd29f6dd049
Enileme <dieu.emeline@gmail.com>
2016-09-01 15:30:10 +0200

    modif save

--- src/Draggenda/Save.java
+++ src/Draggenda/Save.java
@@ -1,5 +1,22 @@
 package Draggenda;
 
+import java.io.PrintWriter;
+
 public class Save {
 
+	String utilisateur;
+	String mdp;
+	
+	public Save(String utilisateur,String mdp){
+		this.utilisateur=utilisateur;
+		this.mdp=mdp;
+		try{
+		PrintWriter save1 = new PrintWriter(utilisateur+".csv");
+		save1.write(utilisateur+" "+mdp);
+		save1.close();
+		}catch(Exception e){
+			System.out.println("Erreur");
+		}
+	}
+	
 }
