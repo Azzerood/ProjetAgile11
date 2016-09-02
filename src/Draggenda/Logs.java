776433b20a176a4dff7f1a045541c678a7810079
azzed <azzed@DESKTOP-3JA9ICU>
2016-09-02 08:21:08 +0200

    Ajout de la classe Log et ses test

---
+++ src/Draggenda/Logs.java
@@ -0,0 +1,69 @@
+package Draggenda;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Logs {
+	HashMap<String, String> comptes = new HashMap<>();
+	
+	public void ajouterCompte(String login, String mdp){
+		comptes.put(login, mdp);
+	}
+	
+	public ArrayList<String> serialiser(){
+	ArrayList<String> serial = new ArrayList<String>();
+	for (String mapKey : comptes.keySet()) {
+		serial.add(""+mapKey+";"+comptes.get(mapKey));
+	}
+	return serial;
+	}
+	
+	public void deserialiser( ArrayList<String> comptes){
+		for(String serial : comptes){
+			boolean loginFini= false;
+			String login ="";
+			String mdp = "";
+			for(int idx=0; idx<serial.length();idx++){
+				
+				if(serial.charAt(idx) != ';'){
+					if(!loginFini){
+						login +=serial.charAt(idx);
+					}else{
+						mdp+=serial.charAt(idx);
+					}
+				}else{
+					loginFini=true;
+				}
+			
+			}
+			this.comptes.put(login, mdp);
+		}
+	}
+	
+	public boolean loginExiste(String login){
+		return comptes.containsKey(login);
+	}
+	public boolean CompteExiste(String login, String mdp){
+		
+		if(!loginExiste(login)){
+			return false;
+		}
+		if(!mdp.equals(comptes.get(login))){
+			return false;
+		}
+		return true;
+	}
+	public void saveComptes(){
+		ArrayList<String> serial = serialiser();
+		//TODO
+		//il faut stocker chaque string de serial dans une ligne du fichier csv
+	}
+	
+	public ArrayList<String> loadComptes(){
+		ArrayList<String> serial = serialiser();
+	
+		//TODO
+		return serial;
+	}
+}
