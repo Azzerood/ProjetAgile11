4bb5ad266067c420f21423ce7f6fc503773bcb63
PC Dylan <PC Dylan@Dylan>
2016-09-02 08:32:15 +0200

    MenuLog

--- src/Draggenda/MenuLog.java
+++ src/Draggenda/MenuLog.java
@@ -1,5 +1,28 @@
 package Draggenda;
 
+import java.util.Scanner;
+
 public class MenuLog {
+		
+	String nom;
+	String motdepasse;
+	Scanner sc= new Scanner(System.in);
+	public MenuLog(){
+		
+	}
 	
+	public String SaisirNom(){
+		System.out.println("Veuillez saisir votre nom d'utilisateur");
+		nom=sc.nextLine();
+		return nom;
+	}
+	public String SaisirMdp(){
+		System.out.println("Veuillez saisir votre mot de passe");
+		motdepasse=sc.nextLine();
+		return motdepasse;
+	}
+	public void LancerMenuLog(){
+		SaisirNom();
+		SaisirMdp();
+	}
 }
