cbc0fe411ebc474aaaacd77d39e2403ff2992c33
Gaby <Gaby@DESKTOP-NQ8TNGG>
2016-09-02 12:03:59 +0200

    test menu

--- test/Draggenda/MenuTest.java
+++ test/Draggenda/MenuTest.java
@@ -11,14 +11,14 @@ public class MenuTest {
 	
 	@Test
 	public void afficherMenu() {
-		assertEquals(new Menu(0).TexteMenu() , "Votre Menu d'actions\n1- Consulter votre agenda\n2- Créer un événement\n3- Recherche d'utilisateur");
+		assertEquals(new Menu(0,new Save(new Logs())).TexteMenu() , "Votre Menu d'actions\n1- Consulter votre agenda\n2- Créer un événement\n3- Recherche d'utilisateur");
 	}
 	
 	@Test
 	public void verifNombre() {
-		assertEquals( new Menu(0).VerifNombre("toto"), -1 );
-		assertEquals( new Menu(0).VerifNombre("8"), 8); 
-		assertEquals( new Menu(0).VerifNombre("1039"), 1039); 
+		assertEquals( new Menu(0,new Save(new Logs())).VerifNombre("toto"), -1 );
+		assertEquals(new Menu(0,new Save(new Logs())).VerifNombre("8"), 8); 
+		assertEquals( new Menu(0,new Save(new Logs())).VerifNombre("1039"), 1039); 
 		
 	}
 	
